b9221e30fd346f1dec998b500446cea975e7c8f8
brucevanhorn <bvanhorn@cleartechnologies.net>
2018-12-28 15:30:47 -0600

    added pytest test file

---
+++ test_app.py
@@ -0,0 +1,24 @@
+from urllib import parse, request
+
+access_token = ""
+base_url = 'http://localhost:5000'
+
+
+def test_user_create():
+    assert False
+
+
+def test_login():
+    assert False
+
+
+def test_create():
+    assert False
+
+
+def test_update():
+    assert False
+
+
+def test_delete():
+    assert False

0468edb45b8af772bc317dae4d248cf7f28797fe
brucevanhorn <bvanhorn@cleartechnologies.net>
2019-01-26 12:47:42 -0600

    modified the test file to use flask's test client

--- test_app.py
+++ test_app.py
@@ -1,7 +1,20 @@
-from urllib import parse, request
+import app
+from flask import json
+
 
 access_token = ""
 base_url = 'http://localhost:5000'
+
+
+def test_super_simple():
+    response = app.test_client().get(
+        '/super_simple'
+    )
+
+    data = json.loads(response.get_data(as_text=True))
+
+    assert response.status_code == 200
+    assert data['message'] == 'Hello from the Planetary API'
 
 
 def test_user_create():

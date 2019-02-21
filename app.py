ed350f1127dc9d3a1b4a3939cea86e16d4e3c2b5
Bruce Van Horn <brucevanhorn@gmail.com>
2019-02-21 05:21:47 -0600

    I spaced things out to make it easier to read.

--- app.py
+++ app.py
@@ -35,20 +35,36 @@ def db_destroy():
 
 @app.cli.command('db_seed')
 def db_seed():
-    mercury = Planet(planet_name='Mercury', planet_type="Class D", home_star="Sol",
-                     mass=3.258e23, radius=1516, distance=35.98e6)
+    mercury = Planet(planet_name='Mercury',
+                     planet_type="Class D",
+                     home_star="Sol",
+                     mass=3.258e23,
+                     radius=1516,
+                     distance=35.98e6)
 
-    venus = Planet(planet_name='Venus', planet_type="Class K", home_star="Sol",
-                   mass=4.867e24, radius=3760, distance=67.24e6)
+    venus = Planet(planet_name='Venus',
+                   planet_type="Class K",
+                   home_star="Sol",
+                   mass=4.867e24,
+                   radius=3760,
+                   distance=67.24e6)
 
-    earth = Planet(planet_name='Earth', planet_type="Class M", home_star="Sol",
-                   mass=5.972e24, radius=3959, distance=92.96e6)
+    earth = Planet(planet_name='Earth',
+                   planet_type="Class M",
+                   home_star="Sol",
+                   mass=5.972e24,
+                   radius=3959,
+                   distance=92.96e6)
 
     db.session.add(mercury)
     db.session.add(venus)
     db.session.add(earth)
 
-    test_user = User(first_name='William', last_name='Herschel', email="test@test.com", password="P@ssw0rd")
+    test_user = User(first_name='William',
+                     last_name='Herschel',
+                     email="test@test.com",
+                     password="P@ssw0rd")
+
     db.session.add(test_user)
 
     db.session.commit()
@@ -111,7 +127,6 @@ def not_found():
 @app.route('/register', methods=['POST'])
 def register():
     email = request.form['email']
-
     test = User.query.filter_by(email=email).first()
 
     if test:
@@ -139,7 +154,7 @@ def retrieve_password(email: str):
         return jsonify(message="That email doesn't exist."), 404
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['POST'])
 def login():
     if request.is_json:
         email = request.json['email']
@@ -214,7 +229,7 @@ def update_planet():
         planet.radius = float(request.form['radius'])
         planet.distance = float(request.form['distance'])
         db.session.commit()
-        return jsonify(message="You updated a planet")
+        return jsonify(message="You updated a planet"), 202
     else:
         return jsonify(message="That planet does not exist"), 404
 
@@ -226,7 +241,7 @@ def remove_planet(planet_id: int):
     if planet:
         db.session.delete(planet)
         db.session.commit()
-        return jsonify(message="You deleted a planet: " + str(planet_id))
+        return jsonify(message="You deleted a planet: " + str(planet_id)), 202
     else:
         return jsonify(message="That planet doesn't exist."), 404
 
